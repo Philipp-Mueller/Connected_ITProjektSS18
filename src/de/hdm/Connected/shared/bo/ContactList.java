e5dcf2f1fa62ab15dbfbd2d4863055dc69ee5530
denisesemmler <semmlerdenise1308@gmail.com>
2018-04-13 15:56:38 +0200

    Klasse ContactList in shared.bo erstellt

---
+++ src/de/hdm/Connected/shared/bo/ContactList.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package de.hdm.Connected.shared.bo;
+
+/**
+ * @author Denise
+ *
+ */
+public class ContactList extends BusinessObject {
+
+	private static final long serialVersionUID = 1L;
+	private String name ="";
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ContactList() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
