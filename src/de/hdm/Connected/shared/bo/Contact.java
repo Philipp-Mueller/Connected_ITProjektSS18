12c4a83d8aebf1f8cf9d374a0498097778ae1c33
denisesemmler <semmlerdenise1308@gmail.com>
2018-04-26 22:53:25 +0200

    ContactList und Email gelöscht, OwnerId hinzugefügt

--- src/de/hdm/Connected/shared/bo/Contact.java
+++ src/de/hdm/Connected/shared/bo/Contact.java
@@ -12,6 +12,7 @@ public class Contact extends BusinessObject{
 	private static final long serialVersionUID = 1L;
 	private String prename ="";
 	private String surname ="";
+	private int ownerId= 0;
 	/**
 	 * @return the surname
 	 */
@@ -24,21 +25,7 @@ public class Contact extends BusinessObject{
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
-	/**
-	 * @return the logInEmail
-	 */
-	public String getLogInEmail() {
-		return logInEmail;
-	}
-	/**
-	 * @param logInEmail the logInEmail to set
-	 */
-	public void setLogInEmail(String logInEmail) {
-		this.logInEmail = logInEmail;
-	}
-	private int contactlistID = 0;
-	private String logInEmail = "";
-	/**
+		/**
 	 * @return the name
 	 */
 	public String getPrename() {
@@ -51,16 +38,16 @@ public class Contact extends BusinessObject{
 		this.prename = prename;
 	}
 	/**
-	 * @return the contactlistID
+	 * @return the ownerId
 	 */
-	public int getContactlistID() {
-		return contactlistID;
+	public int getOwnerId() {
+		return ownerId;
 	}
 	/**
-	 * @param contactlistID the contactlistID to set
+	 * @param ownerId the ownerId to set
 	 */
-	public void setContactlistID(int contactlistID) {
-		this.contactlistID = contactlistID;
+	public void setOwnerId(int ownerId) {
+		this.ownerId = ownerId;
 	}
 	
 }
