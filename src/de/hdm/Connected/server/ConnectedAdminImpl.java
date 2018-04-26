607e882e58515900a46056af2fe2056df9d855af
denisesemmler <semmlerdenise1308@gmail.com>
2018-04-26 20:06:11 +0200

    KLasse ConnectedAdmin und ConnectedAdminImpl erstellt

---
+++ src/de/hdm/Connected/server/ConnectedAdminImpl.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package de.hdm.Connected.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet; //weiß gerade noch nicht wo bzw wie ich sie sonst importieren soll...
+
+/**
+ * Implemetierungsklasse des Interface ConnectedAdmin. Sie enthält die
+ * Applikationslogik, stellt die Zusammenhänge konstistent dar und ist
+ * zuständig um einen geordneten Ablauf zu gewährleisten.
+ * 
+ * @author Denise
+ *
+ */
+public class ConnectedAdminImpl extends RemoteServiceServlet {
+
+	/**
+	 * 
+	 */
+	public ConnectedAdminImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/de/hdm/Connected/shared/ConnectedAdmin.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package de.hdm.Connected.shared;
+
+/**
+ * Schnittstelle für die RPC-Fähige Klasse ConnectedAdminImpl.
+ * @author Denise
+ *
+ */
+public interface ConnectedAdmin {
+
+}
