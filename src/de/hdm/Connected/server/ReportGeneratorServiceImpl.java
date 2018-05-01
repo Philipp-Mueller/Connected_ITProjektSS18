17b3ff9c77a051187fb6213b1b0c19393f6d99ac
Patricia Rodrigues Ribeiro <patty.rori@gmail.com>
2018-05-01 21:45:49 +0200

    Java Datei Report Generator Impl erstellt

---
+++ src/de/hdm/Connected/server/ReportGeneratorServiceImpl.java
@@ -0,0 +1,13 @@
+package de.hdm.Connected.server;
+
+/**
+ * 
+ * @author Patricia
+ * Applikationslogik des Report Generators zur Wiedergabe aller Kontakte, aller Kontakte per Nutzer,
+ * aller geteilten Kontakte, alle geteilte Kontakte per Nutzer, Abfrage von Eigenschaften anhand Aussprägungen.
+ *
+ */
+
+public class ReportGeneratorServiceImpl {
+
+}
