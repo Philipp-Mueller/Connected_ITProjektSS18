50a7f4ffbd248166aa23608b31a98d63f89105e1
Moritz-Bittner <moritz.bit@gmail.com>
2018-05-29 16:34:24 +0200

    ContactListForm bearbeitet

--- src/de/hdm/Connected/client/gui/ContactListForm.java
+++ src/de/hdm/Connected/client/gui/ContactListForm.java
@@ -1,5 +1,9 @@
 package de.hdm.Connected.client.gui;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -10,43 +14,65 @@ import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
+import de.hdm.Connected.client.ClientSideSettings;
+import de.hdm.Connected.shared.bo.ContactList;
 
-/** 
- * Klasse für die Bereistellung eines Formulars zum Anlegen/Bearbeiten einer Kontaltiste
+/**
+ * Klasse für die Bereistellung eines Formulars zum Anlegen/Bearbeiten einer
+ * Kontaltiste
  * 
  * @author Moritz
  *
  */
 
-public class ContactListForm extends Widget{
-	
+public class ContactListForm extends Widget {
+
 	Label nameLabel = new Label("Name:");
 	TextBox nameBox = new TextBox();
 	Label yourContactsLabel = new Label("Deine Kontakte:");
 	ListBox contactListBox = new ListBox();
 	Button addContactButton = new Button("Kontakt hinzufügen");
-	Button createListButton = new Button("Liste erstellen");
-	
+	Button createListButton = new Button("Liste erstellen", new createContactListClickhandler());
+
 	HorizontalPanel namePanel = new HorizontalPanel();
 	HorizontalPanel contactsPanel = new HorizontalPanel();
-	
-	
-	public ContactListForm () {
-		
-		
+
+	public ContactListForm() {
+
 		VerticalPanel topPanel = new VerticalPanel();
-		
-		
-		
+
 		namePanel.add(nameLabel);
 		namePanel.add(nameBox);
-		
+
 		topPanel.add(new HTML("<h2> Neue Kontaktliste erstellen</h2>"));
 		topPanel.add(namePanel);
 		topPanel.add(addContactButton);
 		topPanel.add(createListButton);
-		
+
 		RootPanel.get("content").add(topPanel);
 	}
 
+	private class createContactListClickhandler implements ClickHandler {
+
+		public void onClick(ClickEvent event) {
+
+			ClientSideSettings.getConnectedAdmin().createContactList(nameBox.getText(),
+					new createContactListCallback());
+
+		}
+	};
+
+	private class createContactListCallback implements AsyncCallback<ContactList> {
+
+		public void onFailure(Throwable caught) {
+			Window.alert("Da ist wohl etwas schief gelaufen");
+		}
+
+		public void onSuccess(ContactList result) {
+			Window.alert("Contactlist angelegt");
+			RootPanel.get("Content").clear();
+
+		}
+
+	}
 }
