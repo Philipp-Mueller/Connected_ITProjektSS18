0106a90b2139a40eeec912f66765f3447b7fae8e
Burak Aridag <37343275+Burak-Aridag@users.noreply.github.com>
2018-06-06 01:23:40 +0200

    Footer im Editor angepasst

--- src/de/hdm/Connected/client/Connected_ITProjektSS18.java
+++ src/de/hdm/Connected/client/Connected_ITProjektSS18.java
@@ -8,9 +8,11 @@ import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
@@ -68,5 +70,46 @@ public class Connected_ITProjektSS18 implements EntryPoint {
 		});
 		RootPanel.get("content").add(newContactButton);
 		RootPanel.get("content").add(newContactListButton);
+		
+		HorizontalPanel footer = new HorizontalPanel();
+		Anchor startPage = new Anchor ("Startseite", "Connected_ITProjektSS18.html");
+		HTML copyrightText1 = new HTML(" | ");
+		Anchor reportGeneratorLink = new Anchor (" ReportGenerator", "Connected_ITProjektSS18ReportGenerator.html");
+		HTML copyrightText2 = new HTML(" | © 2018 Connected | ");
+		Anchor impressumLink = new Anchor("Impressum");
+		footer.add(startPage);
+		footer.add(copyrightText1);
+		footer.add(reportGeneratorLink);
+		footer.add(copyrightText2);
+		footer.add(impressumLink);
+		
+		impressumLink.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				RootPanel.get("content").clear();
+				RootPanel.get("content").add(new HTML("<h2>Impressum nach §5 TMG</h2>"
+						+ "<h3>Verantwortlich</h3>"
+						+ "<p>Hochschule der Medien<br />"
+						+ "Nobelstraße 8<br />"
+						+ "70569 Stuttgart<br /></p>"
+						+ "<p><strong>Projektarbeit innerhalb des Studiengangs "
+						+ "Wirtschaftsinformatik und digitale Medien, "
+						+ "IT-Projekt SS 18.</strong></p>"
+						+ "<h3>Projektteam</h3>"
+						+ "<ul><li>xxx</li>"
+						+ "<li>xxx</li>"
+						+ "<li>xxx</li>"
+						+ "<li>xxx</li>"
+						+ "<li>xxx</li>"
+						+ "<li>xxx</li></ul>"
+						+ "<h3>Kontakt</h3>"
+						+ "<p><strong>Telefon:</strong> 0711 8923 10 (Zentrale)</p>"
+						+ "<p><strong>Website:</strong> <a href='http://www.hdm-stuttgart.de' target='_blank'>"
+						+ "www.hdm-stuttgart.de</a></p>"));
+			}
+			
+		});
+		RootPanel.get("footer").add(footer);
 	}
 }
