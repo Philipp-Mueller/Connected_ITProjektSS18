0b738d90de55e554237a3c520775896e078bf079
Viktoriya Alexeyeva <35735563+ViktoriyaAlexeyeva@users.noreply.github.com>
2018-06-26 17:48:59 +0200

    cellList hinzugefügt

--- src/de/hdm/Connected/client/gui/Navigation.java
+++ src/de/hdm/Connected/client/gui/Navigation.java
@@ -1,5 +1,6 @@
 package de.hdm.Connected.client.gui;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,9 +10,11 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.cellview.client.CellList;
 import com.google.gwt.user.cellview.client.CellTree;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DisclosurePanel;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.ListDataProvider;
@@ -19,75 +22,73 @@ import com.google.gwt.view.client.ProvidesKey;
 import com.google.gwt.view.client.SelectionModel;
 import com.google.gwt.view.client.SingleSelectionModel;
 
+import de.hdm.Connected.client.ClientSideSettings;
+import de.hdm.Connected.shared.bo.Contact;
+
 
 public class Navigation extends VerticalPanel {
 	
 	public void onLoad() {
 		
-		//ContactListsTreeViewModel contactListsTreeModel = new ContactListsTreeViewModel();
+		Button myContacts = new Button ("Meine Kontakte");
+		//myContacts.addStyleName("");
 		
-		//CellTree contactListsTree = new CellTree (contactListsTreeModel, null);
-		
-		Button BNewContact = new Button ("Neuen Kontakt anlegen");
-		// BNewContact.addStyleName("");
-		
-		BNewContact.addClickHandler(new ClickHandler() {
-
+		myContacts.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
-				ContactForm addContactForm = new ContactForm();
-			
+				//Kontakte
 			}
 		});
 		
-		Button BNewContactList = new Button ("Neue Kontaktliste anlegen");
-		// BNewContactList.addStyleName("");
+		ContactlistsCellList ccl = new ContactlistsCellList();
 		
-		BNewContactList.addClickHandler(new ClickHandler() {
-		// BNewContactList.addStyleName("");
-			
+		/*Button myContactlists = new Button ("Meine Kontaktlisten");
+		//myContactlists.addStyleName("");
+		
+		myContactlists.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
-				ContactListForm addContactListForm = new ContactListForm();
+				//Kontaktlisten
+			}
+		}); */
+		
+		
+		/*Button sharedContacts = new Button ("Kontakte");
+		//sharedContactlists.addStyleName("");
+		
+		sharedContacts.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				//Kontakte
 			}
 		});
 		
-		Button BMyContacts = new Button ("Meine Kontakte");
-		// BMyContacts.addStyleName("");
+		Button sharedContactlists = new Button ("Kontaktlisten");
+		//sharedContactlists.addStyleName("");
 		
-		BMyContacts.addClickHandler(new ClickHandler() {
-		// BMyContacts.addStyleName("");
-			
+		myContacts.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
-				//Kontakt-Tabelle;
-			
+				//Kontaktlisten
 			}
-		});
-
-		//CellTree?
-		DisclosurePanel myContactLists = new DisclosurePanel("Meine Kontaktlisten");
-			//myContactLists.setContent(contactListsTree);
-	    
-	    Button BSharedContacts = new Button ("Geteilte Kontakte");
-		// BSharedContacts.addStyleName("");
+		}); */
 		
-		BSharedContacts.addClickHandler(new ClickHandler() {
-		// BSharedContacts.addStyleName("");
-			
-			@Override
-			public void onClick(ClickEvent event) {
-				//geteilte Kontakte 
-			}
-		});
-
-			
-		this.add(BNewContact);
-		this.add(BNewContactList);
-		this.add(BMyContacts);
-		this.add(myContactLists);
-		this.add(BSharedContacts);
+		DisclosurePanel clists = new DisclosurePanel();
 		
+		clists.getHeaderTextAccessor().setText("Meine Kontaktlisten");
+		
+		clists.setContent(ccl);
+		
+		
+		/*VerticalPanel sharedPanel = new VerticalPanel();
+		sharedPanel.add(sharedContacts);
+		sharedPanel.add(sharedContactlists);
+		
+		shared.setContent(sharedPanel);*/
+	    	
+		this.add(myContacts);
+		this.add(clists);
+		//this.add(myContactlists);
 			
 	}		
 }
