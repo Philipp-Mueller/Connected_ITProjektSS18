de18f4154ad7c472df5766281c998b100308e779
Viktoriya Alexeyeva <35735563+ViktoriyaAlexeyeva@users.noreply.github.com>
2018-06-30 22:05:43 +0200

    Änderungen Navi

--- src/de/hdm/Connected/client/gui/Navigation.java
+++ src/de/hdm/Connected/client/gui/Navigation.java
@@ -1,11 +1,5 @@
 package de.hdm.Connected.client.gui;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import com.google.gwt.cell.client.Cell;
-import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.cellview.client.CellList;
@@ -18,10 +12,6 @@ import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.view.client.ListDataProvider;
-import com.google.gwt.view.client.ProvidesKey;
-import com.google.gwt.view.client.SelectionModel;
-import com.google.gwt.view.client.SingleSelectionModel;
 
 import de.hdm.Connected.client.ClientSideSettings;
 import de.hdm.Connected.shared.bo.Contact;
@@ -32,71 +22,32 @@ public class Navigation extends VerticalPanel {
 	
 	public void onLoad() {
 		
-		Button myContacts = new Button ("Meine Kontakte");
+		Button cButton = new Button ("Meine Kontakte");
 		//myContacts.addStyleName("");
 		
-		myContacts.addClickHandler(new ClickHandler() {
+		cButton.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
-				ContactsTable ctable = new ContactsTable();
+				RootPanel.get("content").clear();
+				//ContactsTable
 			}
 		});
 		
-		/*
+		ContactlistsCellList cellList = new ContactlistsCellList();
 		
-		Button myContactlists = new Button ("Meine Kontaktlisten");
-		myContactlists.addStyleName("");
+		DisclosurePanel clPanel = new DisclosurePanel();
+		Button headerButton = new Button("Meine Kontaktlisten");
+		clPanel.setHeader(headerButton);
 		
-		myContactlists.addClickHandler(new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				//Kontaktlisten
-			}
-		}); */
+		VerticalPanel vpanel = new VerticalPanel();
+		vpanel.setBorderWidth(1);	    
+	    vpanel.setWidth("200");
+	    
+	    vpanel.add(cellList);
+		clPanel.setContent(vpanel);
 		
-		
-		/* Button sharedContacts = new Button ("Kontakte");
-		//sharedContactlists.addStyleName("");
-		
-		sharedContacts.addClickHandler(new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				//Kontakte
-			}
-		}); */
-		
-		/* Button sharedContactlists = new Button ("Kontaktlisten");
-		sharedContactlists.addStyleName("");
-		
-		myContacts.addClickHandler(new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				//Kontaktlisten
-			}
-		}); */
-		
-		ContactlistsCellList clcl = new ContactlistsCellList();
-		
-		DisclosurePanel clists = new DisclosurePanel("Meine Kontaktlisten");
-		
-		//clists.getHeaderTextAccessor().setText("Meine Kontaktlisten");
-		
-		clists.setContent(clcl);
-		
-		
-		/*VerticalPanel sharedPanel = new VerticalPanel();
-		sharedPanel.add(sharedContacts);
-		sharedPanel.add(sharedContactlists); */
-		
-		//shared.setContent(sharedPanel);
-		
-		this.add(myContacts);
-		this.add(clists);
-		//this.add(myContactlists);		
-		
-//		RootPanel.get("nav").add(sharedPanel);
-//		RootPanel.get("nav").add(myContacts);
-//		RootPanel.get("nav").add(clists);
-//		RootPanel.get("nav").add(myContactlists);
+		this.add(cButton);
+		this.add(clPanel);	
+
 	}		
 }
